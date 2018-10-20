2f010fee4ef2aee9eae48c87138dd73c295dc48a
Max Alva <alva85@gmail.com>
2018-10-19 21:20:11 -0500

    Conexión a la base de datos

---
+++ config/db_config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Created by Max Alva
+* Date: 2018/10/19
+* Time: 19:42:46
+*/
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'blog_php');
